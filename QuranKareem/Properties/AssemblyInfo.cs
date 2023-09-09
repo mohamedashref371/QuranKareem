5aa5971939a02f6148473d16e3a0a873e099fc99
Mohamed Ashref <102254424+mohamedashref371@users.noreply.github.com>
2023-09-09 20:18:17 +0200

    version 1

--- QuranKareem/Properties/AssemblyInfo.cs
+++ QuranKareem/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Quran Kareem")]
+[assembly: AssemblyTitle("Quran Kareem v1.0")]
 [assembly: AssemblyDescription("Quran Kareem")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Family Kings and Queens of Computer")]
