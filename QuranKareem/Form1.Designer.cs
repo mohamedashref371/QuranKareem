98fafadf3be5d976437fb8d7f11588e875339e73
Mohamed Ashref <102254424+mohamedashref371@users.noreply.github.com>
2023-11-17 12:43:48 +0200

    fix error in form

--- QuranKareem/Form1.Designer.cs
+++ QuranKareem/Form1.Designer.cs
@@ -1138,6 +1138,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1100, 910);
+            this.Controls.Add(this.searchList);
             this.Controls.Add(this.wordColorsCheck);
             this.Controls.Add(this.wordColors);
             this.Controls.Add(this.pageZoom);
@@ -1209,7 +1210,6 @@
             this.Controls.Add(this.addNewMoqrea);
             this.Controls.Add(this.srtFile);
             this.Controls.Add(this.spellingErrors);
-            this.Controls.Add(this.searchList);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
