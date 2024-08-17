1f877d3f5a52d6c9e78d93926d39353bb7a1ea6a
Mohamed Ashref <AshrefKing523@Gmail.com>
2024-08-17 19:25:13 +0300

    add colors in olive project

--- QuranKareem/VideoEditor/OliveXmlBuilder.cs
+++ QuranKareem/VideoEditor/OliveXmlBuilder.cs
@@ -10,6 +10,12 @@ namespace QuranKareem
     {
         private static readonly StringBuilder sb = new StringBuilder();
 
+        private static string VideoColor = "r=\"128\" g=\"128\" b=\"192\"";
+        private static string AudioColor = "r=\"128\" g=\"192\" b=\"128\"";
+
+        private static string ImageColor = "r=\"192\" g=\"160\" b=\"128\"";
+        private static string ImageColor2 = "r=\"192\" g=\"128\" b=\"128\"";
+
         public static string Build()
         {
             sb.Clear();
@@ -41,13 +47,14 @@ namespace QuranKareem
         {
             sb.Append($"<sequences><sequence id=\"1\" folder=\"0\" name=\"{ProjectName}\" width=\"{VideoWidth}\" height=\"{VideoHeight}\" framerate=\"{FrameRate}\" afreq=\"{AudioSampleRate}\" alayout=\"3\" open=\"1\" workarea=\"0\" workareaIn=\"0\" workareaOut=\"0\">");
 
-            sb.Append($"<clip id=\"1\" enabled=\"1\" name=\"QuranTemplate\" clipin=\"{(int)(VideoOffsetInSecond * FrameRate)}\" in=\"0\" out=\"{(int)(LengthInSecond * FrameRate)}\" track=\"-2\" media=\"1\" stream=\"0\"></clip><clip id=\"2\" enabled=\"1\" name=\"QuranAudio\" clipin=\"{(int)(AudioOffsetInSecond * FrameRate)}\" in=\"0\" out=\"{(int)(LengthInSecond * FrameRate)}\" track=\"0\" media=\"2\" stream=\"0\"></clip>");
+            sb.Append($"<clip id=\"1\" enabled=\"1\" name=\"QuranTemplate\" clipin=\"{(int)(VideoOffsetInSecond * FrameRate)}\" in=\"0\" out=\"{(int)(LengthInSecond * FrameRate)}\" {VideoColor} track=\"-2\" media=\"1\" stream=\"0\"></clip>");
+            sb.Append($"<clip id=\"2\" enabled=\"1\" name=\"QuranAudio\" clipin=\"{(int)(AudioOffsetInSecond * FrameRate)}\" in=\"0\" out=\"{(int)(LengthInSecond * FrameRate)}\" {AudioColor} track=\"0\" media=\"2\" stream=\"0\"></clip>");
 
             float sumIn = 0, sumOut;
             for (int i = 0; i < ImagesPaths.Count; i++)
             {
                 sumOut = sumIn + AudioTimestamps[i];
-                sb.Append($"<clip id=\"{3 + i}\" enabled=\"1\" name=\"{Path.GetFileName(ImagesPaths[i])}\" clipin=\"0\" in=\"{(int)(sumIn * FrameRate)}\" out=\"{(int)(sumOut * FrameRate)}\" track=\"-4\" media=\"{3 + i}\" stream=\"0\"></clip>");
+                sb.Append($"<clip id=\"{3 + i}\" enabled=\"1\" name=\"{Path.GetFileName(ImagesPaths[i])}\" clipin=\"0\" in=\"{(int)(sumIn * FrameRate)}\" out=\"{(int)(sumOut * FrameRate)}\" {(i % 2 == 0 ? ImageColor : ImageColor2)} track=\"-4\" media=\"{3 + i}\" stream=\"0\"></clip>");
                 sumIn = sumOut;
             }
 
