d3111257685d8ee44cef6c5969ab82a1d75a08db
Mohamed Ashref <AshrefKing523@Gmail.com>
2024-11-07 21:02:12 +0200

    up to version

--- QuranKareem/Properties/AssemblyInfo.cs
+++ QuranKareem/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Quran Reciters Corner")]
 [assembly: AssemblyProduct("Quran Kareem")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2023-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.7.371")]
-[assembly: AssemblyFileVersion("1.4.7.371")]
+[assembly: AssemblyVersion("1.4.8.371")]
+[assembly: AssemblyFileVersion("1.4.8.371")]
